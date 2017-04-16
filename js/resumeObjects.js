cdf2e7d640ba97684b7de39e2d2ed86b504cebc3
Alexey Solonenko <ot2@bk.ru>
2017-04-16 19:26:11 +0200

    refactor: Do lessons 2-3 before last assignment

---
+++ js/resumeObjects.js
@@ -0,0 +1,120 @@
+
+
+/* WORK OBJECT */
+/* WORK OBJECT */
+
+var work = {
+	"employment" : [ 
+		{
+			"status" : "current",
+			"position" : "Electrical and Instrument Engineer",
+			"employer" : "AIM Enterprises \n",
+			"years" : "08.2015 - current",
+			"location" : "Paola, Malta\n"
+		},
+
+		{
+			"status" : "former",
+			"position" : "Senior Maintenance Electrical Engineer",
+			"employer" : "Total SA",
+			"years" : "03.2013 - 08.2015",
+			"location" : "Kharyaga, Arctic, Russia"
+		},
+
+		{
+			"status" : "former",
+			"position" : "Commissioning Lead Electrical Engineer",
+			"employer" : "Dietsmann Technologies Ltd.",
+			"years" : "04.2011 - 11.2012",
+			"location" : "Kharyaga, Arctic, Russia"
+		},
+
+		{
+			"status" : "current",
+			"position" : "Instrumentation Engineer",
+			"employer" : "Inzhener Ltd.",
+			"years" : "07.2009 - 03.2011",
+			"location" : "Krasnoyarsk, Russia"
+		},
+
+		{
+			"status" : "current",
+			"position" : "Automation Technician",
+			"employer" : "MT-Tekhno Ltd.",
+			"years" : "12.2007 - 03.2009",
+			"location" : "Krasnoyarsk, Russia"
+		}
+	]
+}
+
+/* EDUCATION OBJECT */
+/* EDUCATION OBJECT */
+
+var education = {
+	"CPD" : [
+		{
+			"name" : "Udacity",
+			"major" : "Front-End Web Developer",
+			"years" : "04.2017 - current",
+			"city" : "Web"
+		},
+		
+		{
+			"name" : "George Brown College",
+			"major" : "PLC Technician, certificate",
+			"years" : "2012",
+			"city" : "Distant, Toronto, Canada"
+		}
+	],
+	
+	"fundamental" : [
+		{
+			"name" : "Siberian Federal University",
+			"major" : "Industrial Control Systems",
+			"years" : "2002-2007",
+			"city" : "Krasnoyarsk, Russia"
+		},
+
+		{
+			"name" : "Siberian Federal University",
+			"major" : "English Language Translator",
+			"years" : "2003-2007",
+			"city" : "Krasnoyarsk, Russia"
+		},
+		
+		{
+			"name" : "Siberian Federal University",
+			"major" : "Reserve Army Officer, munitions.",
+			"years" : "2003-2007",
+			"city" : "Krasnoyarsk, Russia"
+		}
+	]
+	
+}
+
+/* PROJECT OBJECT */
+/* PROJECT OBJECT */
+/* PROJECT OBJECT */
+
+var projects = {
+	"projects" : [
+		{
+			"name" : "Language school website",
+			"description" : "Website that I made myself",
+			"url" : "studysnami.ru"
+		},
+		
+		{
+			"name" : "Kharyaga Phase 4",
+			"description" : "30 MW expansion of a 30kbpd oil&gas facility",
+			"url" : "NA"
+		},
+		
+		{
+			"name" : "Kharyaga Phase 3",
+			"description" : "Commissioned of electrical systems of a remote 10 kbpd pad",
+			"url" : "NA"
+		}
+	
+	]
+}
