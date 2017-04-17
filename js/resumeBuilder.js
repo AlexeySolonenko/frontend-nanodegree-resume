ba210ffcfac35138bb4dab92359ab8d8aa126cda
Alexey Solonenko <ot2@bk.ru>
2017-04-17 10:07:34 +0200

    refactor: Make work entries using forEach or For
    Done.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -54,4 +54,32 @@ console.log(result);
 
 console.log(bio.metrics);
 
+for (var i = 0; i < 9; i++) {console.log(i);}
+
+work.employment.forEach(function(record){
+	$("#workExperience").append(HTMLworkStart);
+	var formattedWork = HTMLworkEmployer.replace("%data%",record.employer);
+	$("#workExperience").append(formattedWork);
+	console.log(formattedWork);
+	var formattedWork = HTMLworkTitle.replace("%data%",record.position);
+	$("#workExperience").append(formattedWork);
+	console.log(formattedWork);
+	var formattedWork = HTMLworkDates.replace("%data%",record.years);
+	$("#workExperience").append(formattedWork);
+	console.log(formattedWork);
+	var formattedWork = HTMLworkLocation.replace("%data%",record.location);
+	$("#workExperience").append(formattedWork);
+	console.log(formattedWork);
+	var formattedWork = HTMLworkDescription.replace("%data%",record.status);
+	$("#workExperience").append(formattedWork);
+	console.log(formattedWork);
+	
+	var formattedEmployerTitle = HTMLworkEmployer.replace("%data%",record.employer) +
+	" " + HTMLworkTitle.replace("%data%",record.position);
+	console.log(formattedEmployerTitle);
+	$(".work-entry:last").append(formattedEmployerTitle);
+	
+});
+
+
 	
