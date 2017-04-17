40f57749fec3d76028e008f2fb6b611ab325832f
Alexey Solonenko <ot2@bk.ru>
2017-04-17 10:56:26 +0200

    refactor: Study return of functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -56,30 +56,86 @@ console.log(bio.metrics);
 
 for (var i = 0; i < 9; i++) {console.log(i);}
 
-work.employment.forEach(function(record){
-	$("#workExperience").append(HTMLworkStart);
-	var formattedWork = HTMLworkEmployer.replace("%data%",record.employer);
-	$("#workExperience").append(formattedWork);
-	console.log(formattedWork);
-	var formattedWork = HTMLworkTitle.replace("%data%",record.position);
-	$("#workExperience").append(formattedWork);
-	console.log(formattedWork);
-	var formattedWork = HTMLworkDates.replace("%data%",record.years);
-	$("#workExperience").append(formattedWork);
-	console.log(formattedWork);
-	var formattedWork = HTMLworkLocation.replace("%data%",record.location);
-	$("#workExperience").append(formattedWork);
-	console.log(formattedWork);
-	var formattedWork = HTMLworkDescription.replace("%data%",record.status);
-	$("#workExperience").append(formattedWork);
-	console.log(formattedWork);
-	
-	var formattedEmployerTitle = HTMLworkEmployer.replace("%data%",record.employer) +
-	" " + HTMLworkTitle.replace("%data%",record.position);
-	console.log(formattedEmployerTitle);
-	$(".work-entry:last").append(formattedEmployerTitle);
-	
+function displayWork(){
+	work.employment.forEach(function(record){
+		$("#workExperience").append(HTMLworkStart);
+		var formattedWork = HTMLworkEmployer.replace("%data%",record.employer);
+		$("#workExperience").append(formattedWork);
+		console.log(formattedWork);
+		var formattedWork = HTMLworkTitle.replace("%data%",record.position);
+		$("#workExperience").append(formattedWork);
+		console.log(formattedWork);
+		var formattedWork = HTMLworkDates.replace("%data%",record.years);
+		$("#workExperience").append(formattedWork);
+		console.log(formattedWork);
+		var formattedWork = HTMLworkLocation.replace("%data%",record.location);
+		$("#workExperience").append(formattedWork);
+		console.log(formattedWork);
+		var formattedWork = HTMLworkDescription.replace("%data%",record.status);
+		$("#workExperience").append(formattedWork);
+		console.log(formattedWork);
+		
+		var formattedEmployerTitle = HTMLworkEmployer.replace("%data%",record.employer) +
+		" " + HTMLworkTitle.replace("%data%",record.position);
+		console.log(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedEmployerTitle);
+		
+	});
+}
+
+$(document).click(function(loc) {
+  // your code goes here
+  console.log('X : ',loc.pageX,'; Y : ',loc.pageY)
 });
 
+
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Udacity",
+      "title": "Course Developer",
+      "location": "Mountain View, CA",
+      "dates": "Feb 2014 - Current",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LearnBIG",
+      "title": "Software Engineer",
+      "location": "Seattle, WA",
+      "dates": "May 2013 - Jan 2014",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LEAD Academy Charter High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jul 2012 - May 2013",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Stratford High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jun 2009 - Jun 2012",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+};
+
+// Your code goes here! Let me help you get started
+
+function locationizer(work_obj) {
+	var locationsArray = [];
+	work_obj.jobs.forEach(function(job)
+		{
+			locationsArray.push(job.location);
+		});
+		
+	return locationsArray;
+}
+
+// Did locationizer() work? This line will tell you!
+console.log(locationizer(work));
 
 	
